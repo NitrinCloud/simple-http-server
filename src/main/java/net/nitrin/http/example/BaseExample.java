f1ca87d69950386ed330829fa6749bbc42c4787f
Mats <fischerm@nitrin.dev>
2024-10-02 11:46:43 +0200

    Formatting and load port from args

--- src/main/java/net/nitrin/http/example/BaseExample.java
+++ src/main/java/net/nitrin/http/example/BaseExample.java
@@ -1,25 +1,34 @@
 package net.nitrin.http.example;
 
-import net.nitrin.http.HttpServer;
-import net.nitrin.http.response.HttpResponse;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Map;
+import net.nitrin.http.HttpServer;
+import net.nitrin.http.response.HttpResponse;
 
 public class BaseExample {
 
     public static void main(String[] args) throws IOException {
         HttpServer httpServer = new HttpServer();
-        httpServer.bind(new InetSocketAddress(8888));
+        httpServer.bind(new InetSocketAddress(Integer.parseInt(args[0])));
 
         System.out.println("Server listening...");
         httpServer.listen(httpRequest -> {
             System.out.println(httpRequest);
             if (httpRequest.path().equals("/")) {
-                String html = "<html lang=\"en\"><head><title>Test</title></head><body><h1>Hello world!</h1><form method=\"post\"><input type=\"email\" id=\"email\" name=\"email\" /><button type=\"submit\">Submit</button></form></body></html>";
-                return new HttpResponse(httpRequest.protocol(), HttpResponse.StatusCode.OK, Map.of("Content-Type", "text/html",
-                        "Content-Length", String.valueOf(html.length())), html);
+                String html =
+                    "<html lang=\"en\"><head><title>Test</title></head><body><h1>Hello world!</h1><form method=\"post\"><input type=\"email\" id=\"email\" name=\"email\" /><button type=\"submit\">Submit</button></form></body></html>";
+                return new HttpResponse(
+                    httpRequest.protocol(),
+                    HttpResponse.StatusCode.OK,
+                    Map.of(
+                        "Content-Type",
+                        "text/html",
+                        "Content-Length",
+                        String.valueOf(html.length())
+                    ),
+                    html
+                );
             }
             return null;
         });
